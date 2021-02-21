a676213c0128a68beb1f828257255b6fd3f09f23
ovehrichard <omo_rich@yahoo.com>
2021-02-21 07:17:20 +0100

    task2

---
+++ Task 2.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<Html>
+<head>
+<p>Recharge Card Printing for My SideHustle</P>
+</Head>
+<body>
+
+ <?php
+ for ($t=0; $t <199 ; $t++) { 
+     $Recharge_pin = rand(000000000000,999999999999);
+         echo $Recharge_pin;
+ }
+ ?>
+
+</body>
+</html>
